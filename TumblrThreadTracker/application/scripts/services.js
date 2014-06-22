adb01aca8039a6bc92a08111ae3f8c94bc835eba
Rosalind Wills <rosalind.m.wills@gmail.com>
2014-06-22 16:30:43 -0500

    Initial view and resource loading with new angular structure

---
+++ TumblrThreadTracker/application/scripts/services.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/* Services */
+
+
+// Demonstrate how to register services
+// In this case it is a simple value service.
+angular.module('rpThreadTracker.services', [])
+    .service('threadService', [function() {
+
+        function getRecentThreads(count, userId) {
+            
+        }
+
+    return {
+
+    };
+}])
