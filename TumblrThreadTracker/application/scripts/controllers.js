dec95fb7892eb22c7805b238a4b184184c7d411c
Blackjack Kent <blackjacksoftwaredev@gmail.com>
2014-07-10 18:02:23 -0500

    Adding thread status count

--- TumblrThreadTracker/application/scripts/controllers.js
+++ TumblrThreadTracker/application/scripts/controllers.js
@@ -6,7 +6,12 @@ angular.module('rpThreadTracker.controllers', ['rpThreadTracker.services'])
   .controller('DashboardController', ['$scope', 'threadService', function($scope, threadService) {
       function updateThreads(data) {
           $scope.threads = data;
-          console.log(data);
+          $scope.myTurnCount = 0;
+          $scope.theirTurnCount = 0;
+          angular.forEach($scope.threads, function(thread) {
+              $scope.myTurnCount += thread.IsMyTurn ? 1 : 0;
+              $scope.theirTurnCount += thread.IsMyTurn ? 0 : 1;
+          })
       }
 
         threadService.subscribe(updateThreads);
