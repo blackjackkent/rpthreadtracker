6f4bf1c8b36dd07d77a5eda42dbd862f49a55b7b
Rosalind Wills <rosalind.m.wills@gmail.com>
2014-07-19 15:48:31 -0500

    Finishing blog sort filters

--- TumblrThreadTracker/application/scripts/filters.js
+++ TumblrThreadTracker/application/scripts/filters.js
@@ -2,7 +2,18 @@
 
 /* Filters */
 
-angular.module('rpThreadTracker.filters', []).
-  filter('fiveMostRecentThreads', [function() {
-    
-  }]);
+angular.module('rpThreadTracker.filters', [])
+    .filter('isCurrentBlog', function() {
+        return function(threads, blogShortname) {
+            if (blogShortname === null || blogShortname === '') {
+                return threads;
+            }
+            var out = [];
+            for (var i = 0; i < threads.length; i++) {
+                if (threads[i].BlogShortname == blogShortname) {
+                    out.push(threads[i]);
+                }
+            }
+            return out;
+        };
+    });
