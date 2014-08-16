36b211a8076dac0181c906539c500879d928561c
Blackjack Kent <blackjacksoftwaredev@gmail.com>
2014-08-16 15:13:19 -0500

    Login and public/private views

--- TumblrThreadTracker/application/scripts/services.js
+++ TumblrThreadTracker/application/scripts/services.js
@@ -12,7 +12,7 @@ angular.module('rpThreadTracker.services', [])
                         url: '/api/Thread',
                         method: 'GET'
                     },
-                    success = function(response) {
+                    success = function (response) {
                         deferred.resolve(response.data);
                     },
                     error = function(data) {
@@ -71,6 +71,77 @@ angular.module('rpThreadTracker.services', [])
                 getThreads: getThreads
             };
         }
+    ])
+    .service('publicThreadService', [
+    '$q', '$http', function ($q, $http) {
+        var subscribers = [],
+            threads = [];
+
+        function getThreadIds(userId, blogShortname) {
+            var deferred = $q.defer(),
+                config = {
+                    url: '/api/PublicThread?userId=' + userId + "&blogShortname=" + blogShortname,
+                    method: 'GET'
+                },
+                success = function (response) {
+                    deferred.resolve(response.data);
+                },
+                error = function (data) {
+                    deferred.reject(data);
+                };
+            $http(config).then(success).catch(error);
+            return deferred.promise;
+        }
+
+        function getThreads(userId, blogShortname, force) {
+            if (threads.length > 0 && !force) {
+                broadcast(threads);
+                return;
+            }
+            threads = [];
+            getThreadIds(userId, blogShortname).then(function (ids) {
+                angular.forEach(ids, function (value, key) {
+                    getThread(value);
+                });
+            });
+        };
+
+        function getThread(id) {
+            var deferred = $q.defer(),
+                config = {
+                    url: '/api/PublicThread/' + id,
+                    method: 'GET'
+                },
+                success = function (response) {
+                    threads.push(response.data);
+                    broadcast(threads);
+                };
+            $http(config).then(success);
+        };
+
+        function subscribe(callback) {
+            subscribers.push(callback);
+        }
+
+        function unsubscribe(callback) {
+            var index = subscribers.indexOf(callback);
+            if (index > -1) {
+                subscribers.splice(index, 1);
+            }
+        }
+
+        function broadcast(data) {
+            angular.forEach(subscribers, function (callback, key) {
+                callback(data);
+            });
+        }
+
+        return {
+            subscribe: subscribe,
+            unsubscribe: unsubscribe,
+            getThreads: getThreads
+        };
+    }
     ])
     .service('blogService', [
         '$q', '$http', function($q, $http) {
