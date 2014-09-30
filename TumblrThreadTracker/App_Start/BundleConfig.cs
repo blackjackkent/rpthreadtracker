9a482a49e081a6b7820e847b4e6aacf4f3d07d8b
Rosalind Wills <rosalind.m.wills@gmail.com>
2014-09-30 18:54:02 -0500

    Adding bundling and minification to javascript files

--- TumblrThreadTracker/App_Start/BundleConfig.cs
+++ TumblrThreadTracker/App_Start/BundleConfig.cs
@@ -1,5 +1,6 @@
 ﻿using System.Web;
 using System.Web.Optimization;
+using BundleTransformer.Core.Bundles;
 
 namespace TumblrThreadTracker
 {
@@ -8,7 +9,16 @@ namespace TumblrThreadTracker
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            
+            bundles.Add(new CustomScriptBundle("~/bundles/angular-bootstrap").Include(
+                 "~/application/scripts/app.js"));
+            bundles.Add(new CustomScriptBundle("~/bundles/angular-application")
+                .Include("~/application/scripts/directives.js")
+                .Include("~/application/scripts/controllers.js")
+                .Include("~/application/scripts/filters.js")
+                .Include("~/application/scripts/services.js"));
+
+
+            BundleTable.EnableOptimizations = true;
         }
     }
 }
