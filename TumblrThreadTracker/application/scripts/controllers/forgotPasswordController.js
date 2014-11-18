a8bb6280768c1b395bd849cebe0a49743b2a4805
Blackjack Kent <blackjacksoftwaredev@gmail.com>
2014-11-17 18:51:14 -0600

    Improvements to forgot password behavior.

--- TumblrThreadTracker/application/scripts/controllers/forgotPasswordController.js
+++ TumblrThreadTracker/application/scripts/controllers/forgotPasswordController.js
@@ -2,10 +2,12 @@
 var rpThreadTracker = rpThreadTracker || {};
 rpThreadTracker.controllers.controller('ForgotPasswordController', [
     '$scope', '$location', 'sessionService', function($scope, $location, sessionService) {
-        var success = function() {
+        var success = function () {
+                $scope.loading = false;
                 $scope.success = "Success. Check your email box for a temporary password.";
             },
-            fail = function() {
+            fail = function () {
+                $scope.loading = false;
                 $scope.error = "Unknown error. Please try again later.";
             };
         $scope.setBodyClass('signin-page');
@@ -13,6 +15,7 @@ rpThreadTracker.controllers.controller('ForgotPasswordController', [
         $scope.submitForgotPassword = function() {
             $scope.error = "";
             $scope.success = "";
+            $scope.loading = true;
             sessionService.submitForgotPassword($scope.username).then(success, fail);
         };
     }
