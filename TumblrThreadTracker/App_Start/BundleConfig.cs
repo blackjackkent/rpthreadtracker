311ce0ca69fde0af434af7ff7614482774ef208d
Rosalind Wills <rosalind.m.wills@gmail.com>
2014-12-13 15:50:04 -0600

    Adding google analytics

--- TumblrThreadTracker/App_Start/BundleConfig.cs
+++ TumblrThreadTracker/App_Start/BundleConfig.cs
@@ -9,8 +9,8 @@ namespace TumblrThreadTracker
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new CustomScriptBundle("~/bundles/angular-bootstrap").Include(
-                 "~/application/scripts/app.js"));
+            bundles.Add(new CustomScriptBundle("~/bundles/angular-bootstrap").Include("~/application/scripts/app.js"));
+            bundles.Add(new CustomScriptBundle("~/bundles/vendor").Include("~/application/scripts/vendor/*.js"));
             bundles.Add(new CustomScriptBundle("~/bundles/angular-controllers").Include("~/application/scripts/controllers/*.js"));
             bundles.Add(new CustomScriptBundle("~/bundles/angular-directives").Include("~/application/scripts/directives/*.js"));
             bundles.Add(new CustomScriptBundle("~/bundles/angular-filters").Include("~/application/scripts/filters/*.js"));
