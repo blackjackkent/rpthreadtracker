72673661e1a29dc2079454f74e1a8d06488eea80
Rosalind Wills <rosalind.m.wills@gmail.com>
2015-05-02 23:54:33 -0500

    Adding cachebuster

--- TumblrThreadTracker/application/scripts/directives/sideNav.js
+++ TumblrThreadTracker/application/scripts/directives/sideNav.js
@@ -1,9 +1,9 @@
 ﻿'use strict';
 var rpThreadTracker = rpThreadTracker || {};
-rpThreadTracker.directives.directive('sideNav', function() {
+rpThreadTracker.directives.directive('sideNav', ['cacheBuster', function(cacheBuster) {
     return {
         restrict: 'E',
         replace: 'true',
-        templateUrl: '/application/views/directives/side-nav.html'
+        templateUrl: '/application/views/directives/side-nav.html?cacheBuster=' + cacheBuster
     };
-});
+}]);
