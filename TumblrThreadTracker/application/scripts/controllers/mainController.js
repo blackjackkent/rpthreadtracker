99145ac6cb5b33481981577700539c4afbfa4097
Rosalind Wills <rosalind.m.wills@gmail.com>
2015-07-16 21:27:40 -0500

    Finishing bulk archive/untrack

--- TumblrThreadTracker/application/scripts/controllers/mainController.js
+++ TumblrThreadTracker/application/scripts/controllers/mainController.js
@@ -16,6 +16,17 @@ rpThreadTracker.controllers.controller('MainController', [
             });
         }
 
+        function getThreadById(id) {
+            var result;
+            angular.forEach($scope.threads, function (thread) {
+                if (thread.UserThreadId == id) {
+                    result = thread;
+                    return;
+                }
+            });
+            return result;
+        }
+
         $scope.setCurrentBlog = function() {
             contextService.setCurrentBlog($scope.currentBlog);
             $analytics.eventTrack('Change Current Blog', { category: 'Private Thread View' });
@@ -34,6 +45,13 @@ rpThreadTracker.controllers.controller('MainController', [
                 if ($scope.bulkItems.hasOwnProperty(property) && $scope.bulkItems[property] == true) {
                     bulkAffected.push(property);
                 }
+            }
+            if ($scope.bulkItemAction == "UntrackSelected") {
+                $scope.untrackThreads(bulkAffected);
+            } else if ($scope.bulkItemAction == "ArchiveSelected") {
+                $scope.archiveThreads(bulkAffected);
+            } else if ($scope.bulkItemAction == "UnarchiveSelected") {
+                $scope.unarchiveThreads(bulkAffected);
             }
         }
         $scope.pageId = pageId;
@@ -64,29 +82,37 @@ rpThreadTracker.controllers.controller('MainController', [
             $scope.news = news;
         });
 
-        $scope.untrackThread = function (userThreadId, threadTitle) {
+        $scope.untrackThreads = function (userThreadIds) {
             threadService.flushThreads();
-            threadService.untrackThread(userThreadId).then(function() {
+            threadService.untrackThreads(userThreadIds).then(function() {
                 threadService.getThreads();
                 threadService.getArchive();
             });
-            $scope.genericSuccess = threadTitle + " has been untracked.";
+            $scope.genericSuccess = "Threads untracked.";
         };
-        $scope.archiveThread = function(userThreadId, blogShortname, postId, userTitle, watchedShortname) {
+        $scope.archiveThreads = function(userThreadIds) {
             threadService.flushThreads();
-            threadService.editThread(userThreadId, blogShortname, postId, userTitle, watchedShortname, true).then(function () {
+            var threadsToArchive = [];
+            angular.forEach(userThreadIds, function (id) {
+                threadsToArchive.push(getThreadById(id));
+            });
+            threadService.editThreads(threadsToArchive, true).then(function () {
                 threadService.getThreads();
                 threadService.getArchive();
             });
-            $scope.genericSuccess = userTitle + " has been archived.";
+            $scope.genericSuccess = "Threads archived.";
         };
-        $scope.unarchiveThread = function (userThreadId, blogShortname, postId, userTitle, watchedShortname) {
+        $scope.unarchiveThreads = function (userThreadIds) {
             threadService.flushThreads();
-            threadService.editThread(userThreadId, blogShortname, postId, userTitle, watchedShortname, false).then(function() {
+            var threadsToArchive = [];
+            angular.forEach(userThreadIds, function (id) {
+                threadsToArchive.push(getThreadById(id));
+            });
+            threadService.editThreads(threadsToArchive, false).then(function() {
                 threadService.getThreads();
                 threadService.getArchive();
             });
-            $scope.genericSuccess = userTitle + " has been unarchived.";
+            $scope.genericSuccess = "Threads unarchived.";
         };
         $scope.refreshThreads = function () { threadService.getThreads(true); };
         $scope.setDashboardFilter = function(filterString) {
