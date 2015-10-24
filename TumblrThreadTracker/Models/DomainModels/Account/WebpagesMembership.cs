05d9f2a9d5d8b70eca97ad6d6adc99f39b3b66ff
Blackjack Kent <blackjacksoftwaredev@gmail.com>
2015-10-23 20:21:52 -0500

    Fixing nullable bool on WebpagesMembership domain model

--- TumblrThreadTracker/Models/DomainModels/Account/WebpagesMembership.cs
+++ TumblrThreadTracker/Models/DomainModels/Account/WebpagesMembership.cs
@@ -9,7 +9,7 @@ namespace TumblrThreadTracker.Models.DomainModels.Account
         public int UserId { get; set; }
         public DateTime? CreateDate { get; set; }
         public string ConfirmationToken { get; set; }
-        public bool IsConfirmed { get; set; }
+        public bool? IsConfirmed { get; set; }
         public DateTime? LastPasswordFailureDate { get; set; }
         public int PasswordFailuresSinceLastSuccess { get; set; }
         public string Password { get; set; }
