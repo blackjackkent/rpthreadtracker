5baeb743760c35d8b40b01cf5aa2c794f2e3c2ae
Rosalind Wills <rosalind.m.wills@gmail.com>
2016-01-01 15:00:53 -0600

    Disabling forms auth

---
+++ TumblrThreadTracker/Startup.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Web.Http;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OAuth;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Owin;
+using TumblrThreadTracker.Infrastructure;
+using TumblrThreadTracker.Infrastructure.Providers;
+
+[assembly: OwinStartup(typeof(TumblrThreadTracker.Startup))]
+
+namespace TumblrThreadTracker
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureOAuth(app);
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            app.UseWebApi(config);
+        }
+
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = new SimpleAuthorizationServerProvider
+                {
+                    
+                }
+            };
+
+            // Token Generation
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+        }
+    }
+}
