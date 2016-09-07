3b9735885c4952b9bcee7aa68646da0c10bec609
Rosalind Wills <rosalind.m.wills@gmail.com>
2016-09-07 18:09:08 -0500

    Various deployment things

---
+++ TumblrThreadTracker/application/scripts/controllers/maintenanceController.js
@@ -0,0 +1,9 @@
+﻿'use strict';
+var rpThreadTracker = rpThreadTracker || {};
+rpThreadTracker.controllers.controller('StaticController', [
+    '$scope', 'sessionService', 'pageId', function($scope, sessionService, pageId) {
+        $scope.setBodyClass('');
+        $scope.pageId = pageId;
+        $scope.publicView = true;
+    }
+]);
