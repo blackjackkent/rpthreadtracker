30349499b6e6820c302b96367358808829cef723
Rosalind Wills <rosalind.m.wills@gmail.com>
2016-10-31 19:21:44 -0500

    Exporter

--- TumblrThreadTracker/Controllers/ExportController.cs
+++ TumblrThreadTracker/Controllers/ExportController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -38,27 +39,27 @@ namespace TumblrThreadTracker.Controllers
             _tumblrClient = tumblrClient;
         }
 
-        public async Task<HttpResponseMessage> Get()
+        public async Task<HttpResponseMessage> Get([FromUri] bool includeArchived = false)
         {
-            var userId = _webSecurityService.GetCurrentUserIdFromIdentity((ClaimsIdentity)User.Identity);
-            var blogs = _blogService.GetBlogsByUserId(userId, _blogRepository, true);
+            var userId = _webSecurityService.GetCurrentUserIdFromIdentity((ClaimsIdentity) User.Identity);
+            var blogs = _blogService.GetBlogsByUserId(userId, _blogRepository).OrderBy(b => b.BlogShortname);
             var package = new ExcelPackage();
             foreach (var blog in blogs)
             {
                 ExcelWorksheet worksheet = package.Workbook.Worksheets.Add(blog.BlogShortname);
+                var hasThreads = false;
                 //Add the headers
                 worksheet.Cells[1, 1].Value = "Blog Shortname";
                 worksheet.Cells[1, 2].Value = "Post ID";
                 worksheet.Cells[1, 3].Value = "User Title";
                 worksheet.Cells[1, 4].Value = "Watched Shortname";
                 worksheet.Cells[1, 5].Value = "Is Archived";
-                var threadIds = _threadService.GetThreadIdsByBlogId(blog.UserBlogId, _threadRepository);
-                var archivedThreadIds = _threadService.GetThreadIdsByBlogId(blog.UserBlogId, _threadRepository, true);
                 int i = 2;
-                foreach (var threadId in threadIds)
+                var threads = _threadService.GetThreadsByBlog(blog, _threadRepository).OrderBy(t => t.WatchedShortname).ThenBy(t => t.UserTitle);
+                if (threads.Any())
+                    hasThreads = true;
+                foreach (var thread in threads)
                 {
-                    var thread = _threadService.GetById(threadId.GetValueOrDefault(), _blogRepository,
-                        _threadRepository, _tumblrClient, true);
                     worksheet.Cells["A" + i].Value = blog.BlogShortname;
                     worksheet.Cells["B" + i].Value = Convert.ToInt64(thread.PostId);
                     worksheet.Cells["C" + i].Value = thread.UserTitle;
@@ -66,21 +67,34 @@ namespace TumblrThreadTracker.Controllers
                     worksheet.Cells["E" + i].Value = thread.IsArchived;
                     i++;
                 }
-                foreach (var threadId in archivedThreadIds)
+                if (includeArchived)
                 {
-                    var thread = _threadService.GetById(threadId.GetValueOrDefault(), _blogRepository,
-                        _threadRepository, _tumblrClient, true);
-                    worksheet.Cells["A" + i].Value = blog.BlogShortname;
-                    worksheet.Cells["B" + i].Value = Convert.ToInt64(thread.PostId);
-                    worksheet.Cells["C" + i].Value = thread.UserTitle;
-                    worksheet.Cells["D" + i].Value = thread.WatchedShortname;
-                    worksheet.Cells["E" + i].Value = thread.IsArchived;
-                    i++;
+                    var archivedThreads = _threadService.GetThreadsByBlog(blog, _threadRepository, true).OrderBy(t => t.WatchedShortname).ThenBy(t => t.UserTitle);
+                    if (archivedThreads.Any())
+                        hasThreads = true;
+                    foreach (var thread in archivedThreads)
+                    {
+                        worksheet.Cells["A" + i].Value = blog.BlogShortname;
+                        worksheet.Cells["B" + i].Value = Convert.ToInt64(thread.PostId);
+                        worksheet.Cells["C" + i].Value = thread.UserTitle;
+                        worksheet.Cells["D" + i].Value = thread.WatchedShortname;
+                        worksheet.Cells["E" + i].Value = thread.IsArchived;
+                        worksheet.Cells["A" + i + ":E" + i].Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
+                        worksheet.Cells["A" + i + ":E" + i].Style.Fill.BackgroundColor.SetColor(System.Drawing.Color.LightGray);
+                        worksheet.Cells["A" + i + ":E" + i].Style.Font.Color.SetColor(ColorTranslator.FromHtml("#595959"));
+                        i++;
+                    }
+                }
+                if (!hasThreads)
+                {
+                    package.Workbook.Worksheets.Delete(worksheet);
+                    continue;
                 }
                 worksheet.Cells["A1:E1"].Style.Font.Bold = true;
                 worksheet.Cells["A1:E1"].Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
                 worksheet.Cells["A1:E1"].Style.Fill.BackgroundColor.SetColor(System.Drawing.Color.LightBlue);
                 worksheet.Cells["A1:E" + i].AutoFitColumns();
+                worksheet.Cells["B2:B" + i].Style.Numberformat.Format = "0";
             }
             HttpResponseMessage result = new HttpResponseMessage(HttpStatusCode.OK);
             using (MemoryStream ms = new MemoryStream())
@@ -93,7 +107,7 @@ namespace TumblrThreadTracker.Controllers
                 result.Content.Headers.ContentType =
                     new MediaTypeHeaderValue("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
                 result.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment");
-                result.Content.Headers.Add("x-filename", userId + "Export.xlsx");
+                result.Content.Headers.Add("x-filename", userId + "_Export.xlsx");
                 return result;
             }
         }
