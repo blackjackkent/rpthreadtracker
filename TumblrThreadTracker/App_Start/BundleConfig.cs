f6e690ae2ce2c6b2bb1b37aa82e4b01fa71ad312
Rosalind Wills <rosalind.m.wills@gmail.com>
2016-11-22 08:43:41 -0600

    Bundling fix

--- TumblrThreadTracker/App_Start/BundleConfig.cs
+++ TumblrThreadTracker/App_Start/BundleConfig.cs
@@ -8,14 +8,21 @@ namespace TumblrThreadTracker
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new CustomScriptBundle("~/bundles/bower").Include(new string[]
+            {
+                "~/application/scripts/bower/angular/angular.min.js",
+                "~/application/scripts/bower/angular-route/angular-route.min.js",
+                "~/application/scripts/bower/angulartics/dist/angulartics.min.js",
+                "~/application/scripts/bower/angulartics-google-analytics/dist/angulartics-ga.min.js",
+                "~/application/scripts/bower/lodash/dist/lodash.min.js",
+                "~/application/scripts/bower/bootstrap-switch/dist/js/bootstrap-switch.min.js",
+                "~/application/scripts/bower/angular-bootstrap-switch/dist/angular-bootstrap-switch.min.js"
+            }));
             bundles.Add(new CustomScriptBundle("~/bundles/angular-bootstrap").Include("~/application/scripts/app.js"));
-            bundles.Add(
-                new CustomScriptBundle("~/bundles/angular-controllers").Include("~/application/scripts/controllers/*.js"));
-            bundles.Add(
-                new CustomScriptBundle("~/bundles/angular-directives").Include("~/application/scripts/directives/*.js"));
+            bundles.Add(new CustomScriptBundle("~/bundles/angular-controllers").Include("~/application/scripts/controllers/*.js"));
+            bundles.Add(new CustomScriptBundle("~/bundles/angular-directives").Include("~/application/scripts/directives/*.js"));
             bundles.Add(new CustomScriptBundle("~/bundles/angular-filters").Include("~/application/scripts/filters/*.js"));
-            bundles.Add(
-                new CustomScriptBundle("~/bundles/angular-services").Include("~/application/scripts/services/*.js"));
+            bundles.Add(new CustomScriptBundle("~/bundles/angular-services").Include("~/application/scripts/services/*.js"));
 
 
             //BundleTable.EnableOptimizations = true;
