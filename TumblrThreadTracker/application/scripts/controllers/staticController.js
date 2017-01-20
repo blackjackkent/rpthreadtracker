31380ed21f43ccc7daee42a622b575bafa0d0d7d
Rosalind Wills <rosalind.m.wills@gmail.com>
2017-01-20 15:12:46 -0600

    Finalizing ESLint config, headercontroller, logincontroller, logoutcontroller, staticcontroller

--- TumblrThreadTracker/application/scripts/controllers/staticController.js
+++ TumblrThreadTracker/application/scripts/controllers/staticController.js
@@ -1,17 +1,19 @@
-﻿(function() {
-	"use strict";
-	angular.module("rpthreadtracker")
-		.controller("StaticController",
+﻿'use strict';
+(function() {
+	angular.module('rpthreadtracker')
+		.controller('StaticController',
 		[
-			"sessionService", "pageId", '$controller', '$scope', 'BodyClass', staticController
+			'sessionService', 'pageId', '$controller', '$scope', 'BodyClass', staticController
 		]);
 
+	/** @this staticController */
+	// eslint-disable-next-line valid-jsdoc, max-params, max-len
 	function staticController(sessionService, pageId, $controller, $scope, BodyClass) {
 		var vm = this;
-		angular.extend(vm, $controller('BaseController as base', { $scope: $scope }));
+		angular.extend(vm, $controller('BaseController as base', {'$scope': $scope}));
 		sessionService.loadUser(vm);
 		BodyClass.set('');
 		vm.pageId = pageId;
 		vm.publicView = true;
 	}
-})();
+}());
