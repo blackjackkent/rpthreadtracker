1d019df77e01cfe4757dc8090493295f6f3845b4
Blackjack Kent <blackjacksoftwaredev@gmail.com>
2017-02-16 19:37:04 -0600

    Adding unit tests project

---
+++ RPThreadTrackerTests/Models/ServiceModels/PostTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using RPThreadTracker.Models.ServiceModels;
+
+namespace RPThreadTrackerTests.Models.ServiceModels
+{
+	[TestFixture]
+	public class PostTests
+	{
+		[Test]
+		public void MostRecentRelevantNote_NotesNull_ReturnsNull()
+		{
+			// Arrange
+			var post = new Post
+			{
+				Notes = null
+			};
+
+			// Act
+			var result = post.GetMostRecentRelevantNote("test", "test");
+
+			// Assert
+			Assert.That(result, Is.Null);
+		}
+	}
+}
