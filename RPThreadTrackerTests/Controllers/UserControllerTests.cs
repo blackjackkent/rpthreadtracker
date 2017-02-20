00c1347ee79b0a399b9ecdb962c10767d3649532
Blackjack Kent <blackjacksoftwaredev@gmail.com>
2017-02-20 03:59:25 -0600

    Refactoring tests with setup method

--- RPThreadTrackerTests/Controllers/UserControllerTests.cs
+++ RPThreadTrackerTests/Controllers/UserControllerTests.cs
@@ -13,19 +13,29 @@
 	[TestFixture]
 	internal class UserControllerTests
 	{
+		private Mock<IWebSecurityService> _webSecurityService;
+		private Mock<IUserProfileService> _userProfileService;
+		private Mock<IRepository<User>> _userProfileRepository;
+		private UserController _userController;
+
+		[SetUp]
+		public void Setup()
+		{
+			_webSecurityService = new Mock<IWebSecurityService>();
+			_userProfileService = new Mock<IUserProfileService>();
+			_userProfileRepository = new Mock<IRepository<User>>();
+			_userController = new UserController(_webSecurityService.Object, _userProfileService.Object, _userProfileRepository.Object);
+		}
+
 		[Test]
 		public void Get_UserFound_ReturnsUser()
 		{
 			// Arrange
 			var user = new UserBuilder().Build();
-			var webSecurityService = new Mock<IWebSecurityService>();
-			var userService = new Mock<IUserProfileService>();
-			var userProfileRepository = new Mock<IRepository<User>>();
-			webSecurityService.Setup(s => s.GetCurrentUserFromIdentity(It.IsAny<ClaimsIdentity>(), userProfileRepository.Object)).Returns(user);
-			var controller = new UserController(webSecurityService.Object, userService.Object, userProfileRepository.Object);
+			_webSecurityService.Setup(s => s.GetCurrentUserFromIdentity(It.IsAny<ClaimsIdentity>(), _userProfileRepository.Object)).Returns(user);
 
 			// Act
-			var result = controller.Get();
+			var result = _userController.Get();
 
 			// Assert
 			Assert.That(result, Is.TypeOf<OkNegotiatedContentResult<UserDto>>());
@@ -49,19 +59,15 @@
 				Email = user.Email,
 				Username = "test"
 			};
-			var webSecurityService = new Mock<IWebSecurityService>();
-			var userService = new Mock<IUserProfileService>();
-			var userProfileRepository = new Mock<IRepository<User>>();
-			webSecurityService.Setup(s => s.GetCurrentUserFromIdentity(It.IsAny<ClaimsIdentity>(), userProfileRepository.Object)).Returns(user);
-			userService.Setup(s => s.UserExistsWithEmail(It.IsAny<string>(), userProfileRepository.Object)).Returns(true);
-			userService.Setup(s => s.UserExistsWithUsername(It.IsAny<string>(), userProfileRepository.Object)).Returns(false);
-			var controller = new UserController(webSecurityService.Object, userService.Object, userProfileRepository.Object);
+			_webSecurityService.Setup(s => s.GetCurrentUserFromIdentity(It.IsAny<ClaimsIdentity>(), _userProfileRepository.Object)).Returns(user);
+			_userProfileService.Setup(s => s.UserExistsWithEmail(It.IsAny<string>(), _userProfileRepository.Object)).Returns(true);
+			_userProfileService.Setup(s => s.UserExistsWithUsername(It.IsAny<string>(), _userProfileRepository.Object)).Returns(false);
 
 			// Act
-			var result = controller.Post(request);
+			var result = _userController.Post(request);
 
 			// Assert
-			webSecurityService.Verify(s => s.CreateAccount(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), userProfileRepository.Object), Times.Never());
+			_webSecurityService.Verify(s => s.CreateAccount(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), _userProfileRepository.Object), Times.Never());
 			Assert.That(result, Is.TypeOf<BadRequestErrorMessageResult>());
 			var response = result as BadRequestErrorMessageResult;
 			Assert.That(response.Message, Is.EqualTo("An account with some or all of this information already exists."));
@@ -77,19 +83,15 @@
 				Email = user.Email,
 				Username = "test"
 			};
-			var webSecurityService = new Mock<IWebSecurityService>();
-			var userService = new Mock<IUserProfileService>();
-			var userProfileRepository = new Mock<IRepository<User>>();
-			webSecurityService.Setup(s => s.GetCurrentUserFromIdentity(It.IsAny<ClaimsIdentity>(), userProfileRepository.Object)).Returns(user);
-			userService.Setup(s => s.UserExistsWithEmail(It.IsAny<string>(), userProfileRepository.Object)).Returns(false);
-			userService.Setup(s => s.UserExistsWithUsername(It.IsAny<string>(), userProfileRepository.Object)).Returns(true);
-			var controller = new UserController(webSecurityService.Object, userService.Object, userProfileRepository.Object);
+			_webSecurityService.Setup(s => s.GetCurrentUserFromIdentity(It.IsAny<ClaimsIdentity>(), _userProfileRepository.Object)).Returns(user);
+			_userProfileService.Setup(s => s.UserExistsWithEmail(It.IsAny<string>(), _userProfileRepository.Object)).Returns(false);
+			_userProfileService.Setup(s => s.UserExistsWithUsername(It.IsAny<string>(), _userProfileRepository.Object)).Returns(true);
 
 			// Act
-			var result = controller.Post(request);
+			var result = _userController.Post(request);
 
 			// Assert
-			webSecurityService.Verify(s => s.CreateAccount(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), userProfileRepository.Object), Times.Never());
+			_webSecurityService.Verify(s => s.CreateAccount(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), _userProfileRepository.Object), Times.Never());
 			Assert.That(result, Is.TypeOf<BadRequestErrorMessageResult>());
 			var response = result as BadRequestErrorMessageResult;
 			Assert.That(response.Message, Is.EqualTo("An account with some or all of this information already exists."));
@@ -107,20 +109,16 @@
 				Password = "TestPassword",
 				ConfirmPassword = "TestPassword"
 			};
-			var webSecurityService = new Mock<IWebSecurityService>();
-			var userService = new Mock<IUserProfileService>();
-			var userProfileRepository = new Mock<IRepository<User>>();
-			webSecurityService.Setup(s => s.GetCurrentUserFromIdentity(It.IsAny<ClaimsIdentity>(), userProfileRepository.Object)).Returns(user);
-			userService.Setup(s => s.UserExistsWithEmail(It.IsAny<string>(), userProfileRepository.Object)).Returns(false);
-			userService.Setup(s => s.UserExistsWithUsername(It.IsAny<string>(), userProfileRepository.Object)).Returns(false);
-			webSecurityService.Setup(s => s.CreateAccount(request.Username, request.Password, request.Email, userProfileRepository.Object)).Returns(user.ToDto());
-			var controller = new UserController(webSecurityService.Object, userService.Object, userProfileRepository.Object);
+			_webSecurityService.Setup(s => s.GetCurrentUserFromIdentity(It.IsAny<ClaimsIdentity>(), _userProfileRepository.Object)).Returns(user);
+			_userProfileService.Setup(s => s.UserExistsWithEmail(It.IsAny<string>(), _userProfileRepository.Object)).Returns(false);
+			_userProfileService.Setup(s => s.UserExistsWithUsername(It.IsAny<string>(), _userProfileRepository.Object)).Returns(false);
+			_webSecurityService.Setup(s => s.CreateAccount(request.Username, request.Password, request.Email, _userProfileRepository.Object)).Returns(user.ToDto());
 
 			// Act
-			var result = controller.Post(request);
+			var result = _userController.Post(request);
 
 			// Assert
-			webSecurityService.Verify(bs => bs.CreateAccount(request.Username, request.Password, request.Email, userProfileRepository.Object), Times.Once());
+			_webSecurityService.Verify(bs => bs.CreateAccount(request.Username, request.Password, request.Email, _userProfileRepository.Object), Times.Once());
 			Assert.That(result, Is.TypeOf<CreatedAtRouteNegotiatedContentResult<UserDto>>());
 			var content = result as CreatedAtRouteNegotiatedContentResult<UserDto>;
 			Assert.That(content, Is.Not.Null);
@@ -134,17 +132,11 @@
 		[Test]
 		public void Put_RequestNull_ReturnsBadRequest()
 		{
-			// Arrange
-			var webSecurityService = new Mock<IWebSecurityService>();
-			var userService = new Mock<IUserProfileService>();
-			var userProfileRepository = new Mock<IRepository<User>>();
-			var controller = new UserController(webSecurityService.Object, userService.Object, userProfileRepository.Object);
-
 			// Act
-			var result = controller.Put(null);
+			var result = _userController.Put(null);
 
 			// Assert
-			userService.Verify(us => us.Update(It.IsAny<UserDto>(), userProfileRepository.Object), Times.Never());
+			_userProfileService.Verify(us => us.Update(It.IsAny<UserDto>(), _userProfileRepository.Object), Times.Never());
 			Assert.That(result, Is.TypeOf<BadRequestResult>());
 		}
 
@@ -153,17 +145,13 @@
 		{
 			// Arrange
 			var user = new UserBuilder().BuildDto();
-			var webSecurityService = new Mock<IWebSecurityService>();
-			var userService = new Mock<IUserProfileService>();
-			var userProfileRepository = new Mock<IRepository<User>>();
-			webSecurityService.Setup(s => s.GetCurrentUserFromIdentity(It.IsAny<ClaimsIdentity>(), userProfileRepository.Object)).Returns((User)null);
-			var controller = new UserController(webSecurityService.Object, userService.Object, userProfileRepository.Object);
+			_webSecurityService.Setup(s => s.GetCurrentUserFromIdentity(It.IsAny<ClaimsIdentity>(), _userProfileRepository.Object)).Returns((User)null);
 
 			// Act
-			var result = controller.Put(user);
+			var result = _userController.Put(user);
 
 			// Assert
-			userService.Verify(us => us.Update(It.IsAny<UserDto>(), userProfileRepository.Object), Times.Never());
+			_userProfileService.Verify(us => us.Update(It.IsAny<UserDto>(), _userProfileRepository.Object), Times.Never());
 			Assert.That(result, Is.TypeOf<BadRequestResult>());
 		}
 
@@ -177,17 +165,13 @@
 			var currentUser = new UserBuilder()
 				.WithUserId(2)
 				.Build();
-			var webSecurityService = new Mock<IWebSecurityService>();
-			var userService = new Mock<IUserProfileService>();
-			var userProfileRepository = new Mock<IRepository<User>>();
-			webSecurityService.Setup(s => s.GetCurrentUserFromIdentity(It.IsAny<ClaimsIdentity>(), userProfileRepository.Object)).Returns(currentUser);
-			var controller = new UserController(webSecurityService.Object, userService.Object, userProfileRepository.Object);
+			_webSecurityService.Setup(s => s.GetCurrentUserFromIdentity(It.IsAny<ClaimsIdentity>(), _userProfileRepository.Object)).Returns(currentUser);
 
 			// Act
-			var result = controller.Put(updatedUser);
+			var result = _userController.Put(updatedUser);
 
 			// Assert
-			userService.Verify(us => us.Update(It.IsAny<UserDto>(), userProfileRepository.Object), Times.Never());
+			_userProfileService.Verify(us => us.Update(It.IsAny<UserDto>(), _userProfileRepository.Object), Times.Never());
 			Assert.That(result, Is.TypeOf<BadRequestResult>());
 		}
 
@@ -201,17 +185,13 @@
 			var currentUser = new UserBuilder()
 				.WithUserId(1)
 				.Build();
-			var webSecurityService = new Mock<IWebSecurityService>();
-			var userService = new Mock<IUserProfileService>();
-			var userProfileRepository = new Mock<IRepository<User>>();
-			webSecurityService.Setup(s => s.GetCurrentUserFromIdentity(It.IsAny<ClaimsIdentity>(), userProfileRepository.Object)).Returns(currentUser);
-			var controller = new UserController(webSecurityService.Object, userService.Object, userProfileRepository.Object);
+			_webSecurityService.Setup(s => s.GetCurrentUserFromIdentity(It.IsAny<ClaimsIdentity>(), _userProfileRepository.Object)).Returns(currentUser);
 
 			// Act
-			var result = controller.Put(updatedUser);
+			var result = _userController.Put(updatedUser);
 
 			// Assert
-			userService.Verify(us => us.Update(updatedUser, userProfileRepository.Object), Times.Once());
+			_userProfileService.Verify(us => us.Update(updatedUser, _userProfileRepository.Object), Times.Once());
 			Assert.That(result, Is.TypeOf<OkResult>());
 		}
 
@@ -219,17 +199,13 @@
 		public void ChangePassword_UserNotFound_ReturnsBadRequest()
 		{
 			// Arrange
-			var webSecurityService = new Mock<IWebSecurityService>();
-			var userService = new Mock<IUserProfileService>();
-			var userProfileRepository = new Mock<IRepository<User>>();
-			webSecurityService.Setup(s => s.GetCurrentUserFromIdentity(It.IsAny<ClaimsIdentity>(), userProfileRepository.Object)).Returns((User)null);
-			var controller = new UserController(webSecurityService.Object, userService.Object, userProfileRepository.Object);
+			_webSecurityService.Setup(s => s.GetCurrentUserFromIdentity(It.IsAny<ClaimsIdentity>(), _userProfileRepository.Object)).Returns((User)null);
 
 			// Act
-			var result = controller.ChangePassword(new ChangePasswordRequest());
+			var result = _userController.ChangePassword(new ChangePasswordRequest());
 
 			// Assert
-			webSecurityService.Verify(s => s.ChangePassword(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()), Times.Never());
+			_webSecurityService.Verify(s => s.ChangePassword(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()), Times.Never());
 			Assert.That(result, Is.TypeOf<BadRequestResult>());
 		}
 
@@ -238,18 +214,14 @@
 		{
 			// Arrange
 			var user = new UserBuilder().Build();
-			var webSecurityService = new Mock<IWebSecurityService>();
-			var userService = new Mock<IUserProfileService>();
-			var userProfileRepository = new Mock<IRepository<User>>();
-			webSecurityService.Setup(s => s.GetCurrentUserFromIdentity(It.IsAny<ClaimsIdentity>(), userProfileRepository.Object)).Returns(user);
-			webSecurityService.Setup(s => s.ChangePassword(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>())).Returns(false);
-			var controller = new UserController(webSecurityService.Object, userService.Object, userProfileRepository.Object);
+			_webSecurityService.Setup(s => s.GetCurrentUserFromIdentity(It.IsAny<ClaimsIdentity>(), _userProfileRepository.Object)).Returns(user);
+			_webSecurityService.Setup(s => s.ChangePassword(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>())).Returns(false);
 
 			// Act
-			var result = controller.ChangePassword(new ChangePasswordRequest());
+			var result = _userController.ChangePassword(new ChangePasswordRequest());
 
 			// Assert
-			webSecurityService.Verify(s => s.ChangePassword(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()), Times.Once());
+			_webSecurityService.Verify(s => s.ChangePassword(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()), Times.Once());
 			Assert.That(result, Is.TypeOf<BadRequestResult>());
 		}
 
@@ -258,18 +230,14 @@
 		{
 			// Arrange
 			var user = new UserBuilder().Build();
-			var webSecurityService = new Mock<IWebSecurityService>();
-			var userService = new Mock<IUserProfileService>();
-			var userProfileRepository = new Mock<IRepository<User>>();
-			webSecurityService.Setup(s => s.GetCurrentUserFromIdentity(It.IsAny<ClaimsIdentity>(), userProfileRepository.Object)).Returns(user);
-			webSecurityService.Setup(s => s.ChangePassword(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>())).Returns(true);
-			var controller = new UserController(webSecurityService.Object, userService.Object, userProfileRepository.Object);
+			_webSecurityService.Setup(s => s.GetCurrentUserFromIdentity(It.IsAny<ClaimsIdentity>(), _userProfileRepository.Object)).Returns(user);
+			_webSecurityService.Setup(s => s.ChangePassword(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>())).Returns(true);
 
 			// Act
-			var result = controller.ChangePassword(new ChangePasswordRequest());
+			var result = _userController.ChangePassword(new ChangePasswordRequest());
 
 			// Assert
-			webSecurityService.Verify(s => s.ChangePassword(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()), Times.Once());
+			_webSecurityService.Verify(s => s.ChangePassword(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()), Times.Once());
 			Assert.That(result, Is.TypeOf<OkResult>());
 		}
 	}
