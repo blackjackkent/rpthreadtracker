022e9a8d1b9c5cbff0de1df06c5fbada6824787c
Rosalind Wills <rosalind.m.wills@gmail.com>
2017-02-20 23:53:08 -0600

    Adding configuration service

---
+++ RPThreadTracker/Infrastructure/Services/ConfigurationService.cs
@@ -0,0 +1,29 @@
+﻿namespace RPThreadTracker.Infrastructure.Services
+{
+	using System.Collections.Generic;
+	using System.Configuration;
+	using System.Linq;
+	using Interfaces;
+
+	/// <inheritdoc cref="IConfigurationService"/>
+	public class ConfigurationService : IConfigurationService
+	{
+		/// <inheritdoc cref="IConfigurationService"/>
+		public bool MaintenanceMode => ConfigurationManager.AppSettings["MaintenanceMode"] == "true";
+
+		/// <inheritdoc cref="IConfigurationService"/>
+		public string NewsBlogShortname => ConfigurationManager.AppSettings["NewsBlogShortname"];
+
+		/// <inheritdoc cref="IConfigurationService"/>
+		public string EmailFromAddress => ConfigurationManager.AppSettings["EmailFromAddress"];
+
+		/// <inheritdoc cref="IConfigurationService"/>
+		public List<string> AllowedMaintenanceIPs => ConfigurationManager.AppSettings["AllowedMaintenanceIPs"].Split(',').ToList();
+
+		/// <inheritdoc cref="IConfigurationService"/>
+		public string TumblrApiKey => ConfigurationManager.AppSettings["TumblrAPIKey"];
+
+		/// <inheritdoc cref="IConfigurationService"/>
+		public string SendGridApiKey => ConfigurationManager.AppSettings["SendGridAPIKey"];
+	}
+}
